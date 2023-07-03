f2c8b1df0a3cf6c1af89f61e393f63e7d027dc84
Seunggyun <s.han@aresa.io>
2023-07-04 08:03:38 +0900

    write test cases for endpoints testing

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,3 +1,37 @@
+import supertest from 'supertest';
+import app from '../index';
+
+const request = supertest(app);
+
+// Endpoint Testing
+
+describe('Test status of responses from the endpoints', (): void => {
+  it('Should Be ok: endpoint: /', async (): Promise<void> => {
+    const response = await request.get('/');
+    expect(response.status).toBe(200);
+  });
+
+  it('Should Be ok: endpoint: /api', async (): Promise<void> => {
+    const response = await request.get('/api');
+    expect(response.status).toBe(200);
+  });
+
+  it('Should Be ok: endpoint: /api/image-resize', async (): Promise<void> => {
+    const response = await request.get('/api/image-resize');
+    expect(response.status).toBe(200);
+  });
+
+  it('Should Be ok: endpoint: /api/placeholder', async (): Promise<void> => {
+    const response = await request.get('/api/placeholder');
+    expect(response.status).toBe(200);
+  });
+
+  it('Should failed: endpoint: /fail', async (): Promise<void> => {
+    const response = await request.get('/fail');
+    expect(response.status).toBe(200);
+  });
+});
+
 // 파일이 존재하지 않을 때 -> 실패
 
 // 파일이 존재하지만 이미지가 아닐 때 -> 실패
