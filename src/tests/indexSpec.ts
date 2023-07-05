25776ba7663d4f5ee48a7d889e9b42e9433463c4
Seunggyun <s.han@aresa.io>
2023-07-05 18:16:17 +0900

    :bug: revise the last test case -image resizer

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,5 +1,10 @@
 import supertest from 'supertest';
 import app from '../index';
+import fs from 'fs';
+import path from 'path';
+
+import { validateFilename, validateSize } from '../utilities/validateParams';
+import { imageResizer } from '../utilities/imageResizer';
 
 const request = supertest(app);
 
@@ -34,7 +39,7 @@ describe('Test status of responses from the endpoints', (): void => {
   });
 });
 
-describe('Testing file handling', (): void => {
+describe('Testing file handling with api', (): void => {
   it('Should Be ok - image-resize: file content type should be an image', async () => {
     const response = await request.get(
       '/api/image-resize?width=300&height=500&filename=cat2.jpg'
@@ -59,5 +64,45 @@ describe('Testing file handling', (): void => {
       '/api/placeholder?width=-300&height=500'
     );
     expect(response.text).toBe('please write a validated size');
+  });
+});
+
+describe('Testing utils', (): void => {
+  it('should Be ok - wrong width: character', () => {
+    const result = validateSize('apt');
+    expect(result).toBe(false);
+  });
+
+  it('should Be ok - wrong width - negative int', () => {
+    const result = validateSize('-300');
+    expect(result).toBe(false);
+  });
+
+  it('should Be ok - correct width', () => {
+    const result = validateSize('300');
+    expect(result).toBe(true);
+  });
+
+  it('should Be ok - wrong filename', () => {
+    const result = validateFilename('seunggyun.jpg');
+    expect(result).toBe(false);
+  });
+
+  it('should Be ok - correct filename', () => {
+    const result = validateFilename('cat2.jpg');
+    expect(result).toBe(true);
+  });
+
+  it('should Be ok - image resize', async () => {
+    imageResizer({ width: 400, height: 400, input: 'cat2.jpg' }).then(() => {
+      const files = fs.readdirSync(
+        path.join(__dirname, '../../assets/resized')
+      );
+      const result =
+        files.find(file => file === 'cat2_400_400.png') !== undefined
+          ? true
+          : false;
+      expect(result).toBe(true);
+    });
   });
 });
